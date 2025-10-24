5a860f85a953d52a07eea3aa58f9c149f3712c9a
Sze Wing Pang <davidpangsw@hotmail.com>
2025-10-24 01:52:31 -0700

    fixing sunflower...

--- dist/sunflower.py
+++ dist/sunflower.py
@@ -1,50 +1,41 @@
 from prototype import PREPARE, TEND, POS, SIZE
-from utils import wait_for_seed, wait_for_harvest, move_to
+from utils import wait_for_seed, wait_for_harvest, move_to, rect_path_even
 from heapq import heappop, heappush
 
 E = Entities.Sunflower
 PQ = "priority queue"
+PATH = "path"
 def prepare(inst):
     x0, y0 = inst[POS]
     w, h = inst[SIZE]
     pq = inst[PQ]
-    for i in range(w):
-        for j in range(h):
-            if can_harvest():
-                harvest()
-            
-            if get_ground_type() != Grounds.Soil:
-                till()
+    path = inst[PATH]
+    for d in path:
+        if can_harvest():
+            harvest()
+        
+        if get_ground_type() != Grounds.Soil:
+            till()
 
-            wait_for_seed(E, 100)
-            plant(E)
-            heappush(pq, (-measure(), (get_pos_x(), get_pos_y())))
-            move(North)
-        for j in range(h):
-            move(South)
-        move(East)
-
-    for i in range(w):
-        move(West)
+        wait_for_seed(E, 100)
+        plant(E)
+        heappush(pq, (-measure(), (get_pos_x(), get_pos_y())))
+        move(d)
 
 def replant(inst):
     x0, y0 = inst[POS]
     w, h = inst[SIZE]
     pq = inst[PQ]
-    for i in range(w):
-        for j in range(h):
-            if get_entity_type() == Entities.sunflower:
-                continue
-            wait_for_seed(E, 100)
-            plant(E)
-            heappush(pq, (-measure(), (get_pos_x(), get_pos_y())))
-            move(North)
-        for j in range(h):
-            move(South)
-        move(East)
+    path = inst[PATH]
 
-    for i in range(w):
-        move(West)
+    move_to((x0, y0))
+    for d in path:
+        if get_entity_type() == Entities.sunflower:
+            continue
+        wait_for_seed(E, 100)
+        plant(E)
+        heappush(pq, (-measure(), (get_pos_x(), get_pos_y())))
+        move(d)
 
 def tend(inst):
     pq = inst[PQ]
@@ -67,9 +58,15 @@ def tend(inst):
 
 # width, height in size should be positive
 def create(pos, size):
+    w, h = size
+    if w % 2 == 1:
+        w -= 1
+        Debug.log("Warning: width not even, decreased by 1")
+        size = w, h
     inst = {
         PREPARE: prepare,
         TEND: tend,
+        PATH: rect_path_even(size),
 
         POS: pos,
         SIZE: size,
