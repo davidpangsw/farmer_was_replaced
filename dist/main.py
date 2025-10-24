0fb99d05423c42014e77944b22b9d0a4a2e14904
Sze Wing Pang <davidpangsw@hotmail.com>
2025-10-24 00:15:20 -0700

    change farm layout

--- dist/main.py
+++ dist/main.py
@@ -2,6 +2,8 @@ import cactus
 import gbtc
 import sunflower
 import pumpkin
+from prototype import POS, PREPARE, TEND
+from utils import move_to
 
 
 clear()
@@ -23,7 +25,11 @@ def spawn_drone_main(inst):
         drone_main(inst)
     spawn_drone(f)
 
-spawn_drone_main(cactus.create_square((0, 0), 10))
-spawn_drone_main(gbtc.create((0, 10), (10, L-10)))
-spawn_drone_main(sunflower.create((10, 0), (L-10, L-6)))
-drone_main(pumpkin.create((L-6, L-6), 6))
+spawn_drone_main(sunflower.create((0, 0), (10, 2)))
+spawn_drone_main(cactus.create_square((0, 2), 10))
+spawn_drone_main(gbtc.create((0, 12), (10, L-12)))
+
+
+spawn_drone_main(pumpkin.create((11, 0), 6))
+spawn_drone_main(gbtc.create((11, 6), (6, L-6)))
+drone_main(gbtc.create((17, 0), (L-16, L)))
