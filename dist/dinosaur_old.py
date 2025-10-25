a1247c8570d820fd64a1451816a1738cf69e9a0f
Sze Wing Pang <davidpangsw@hotmail.com>
2025-10-24 18:31:02 -0700

    fixing dinosaur

--- dist/dinosaur_old.py
+++ dist/dinosaur_old.py
@@ -1,53 +1,54 @@
-from prototype import POS, SIZE
-from utils import rect_path_even, move_to, is_contain
 
-PATH = "path"
-PATH_INDEX = "path_index"
-COUNT = "count"
-def step(inst):
+
+def prepare(inst):
     path = inst[PATH]
-    index = inst[PATH_INDEX]
-    success = move(path[index])
-    inst[PATH_INDEX] = (index + 1) % len(path)
-    return success
-
-def prepare():
     change_hat(Hats.Dinosaur_Hat)
-    step(inst)
-    
 
 def tend(inst):
-    pos = inst[POS]
-    size = inst[SIZE]
-    m = measure()
-    if is_contain(pos, size, m):
-        while (get_pos_x(), get_pos_y()) != m:
-            step(inst)
-        step(inst)
-        inst[COUNT] += 1
-        if inst[COUNT] == len(inst[PATH]):
+    path = inst[PATH]
+    for d in path:
+        success = move(d)
+        if not success:
             change_hat(Hats.Gold_Hat)
             change_hat(Hats.Dinosaur_Hat)
-            inst[COUNT] = 0
+            move(d)
 
-def create():
-    pass
+def create(pos, size):
+    width, height = size
+    if width % 2 == 1:
+        quick_print("Warning: width is not even, increased by 1")
+        width += 1
+    size = (width, width)
 
+    path = rect_path_even(size)
+
+    inst = {
+        PREPARE: prepare,
+        TEND: tend,
+
+        POS: pos,
+        SIZE: size,
+        PATH: path,
+    }
+
+    return inst
 
 def test():
     clear()
-    set_world_size(6)
+    
     L = get_world_size()
-    size = L, L
-    path = rect_path_even(size)
-    change_hat(Hats.Dinosaur_Hat)
-    while True:
-        for d in path:
-            success = move(d)
-            if not success:
-                change_hat(Hats.Gold_Hat)
-                change_hat(Hats.Dinosaur_Hat)
-                move(d)
+    D = max_drones()
+    size = (8, 4)
+
+    width, height = size
+    for x in range(0, L - width, width):
+        for y in range(0, L - height, height):
+            inst = create((x, y), size)
+
+            spawn_drone_main(inst) # last one would fail
+    inst = create((L-width, L-height), size)
+    drone_main(inst)
+    
 
 if __name__ == "__main__":
     test()
