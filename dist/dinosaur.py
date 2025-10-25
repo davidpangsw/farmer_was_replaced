88b54a6d8046780eb8e0aa1e1e3217f726c88011
Sze Wing Pang <davidpangsw@hotmail.com>
2025-10-24 19:05:06 -0700

    fixing dinosaur

--- dist/dinosaur.py
+++ dist/dinosaur.py
@@ -7,18 +7,51 @@ import gbtc
 
 clear()
 
+LENGTH = "length"
+APPLE_POS = "apple position"
+
+# max_size must have even width
+def decide_path(src, dest, max_size, min_len):
+    if src == dest: # only when apple is spawned?
+        return [East, North, West, South]
+    W, H = max_size
+    w, h = dest[0] - src[0], dest[1] - src[1]
+
+    while w * h < min_len and w < W:
+        w += 1
+    while w * h < min_len and h < H:
+        h += 1
+    if w % 2 == 1:
+        w += 1 # this is safe, because max_size must have even width
+    return rect_path_even((w, h))
+    
+
 def prepare(inst):
-    path = inst[PATH]
     change_hat(Hats.Dinosaur_Hat)
+    inst[APPLE_POS] = get_pos_x(), get_pos_y()
 
 def tend(inst):
-    path = inst[PATH]
+    cur_pos = get_pos_x(), get_pos_y()
+    apple_pos = inst[APPLE_POS]
+    length = inst[LENGTH]
+    size = inst[SIZE]
+    path = decide_path(cur_pos, apple_pos, size, length + 1)
     for d in path:
-        success = move(d)
-        if not success:
-            change_hat(Hats.Gold_Hat)
-            change_hat(Hats.Dinosaur_Hat)
-            move(d)
+        entity = get_entity_type() # see if it is apple
+        if entity == Entities.Apple:
+            inst[APPLE_POS] = measure()
+            move(d) # must be success
+            inst[LENGTH] = length + 1
+        else:
+            success = move(d)
+            if not success:
+                change_hat(Hats.Gold_Hat)
+                wait_for_seed(Entities.Apple, 100)
+                change_hat(Hats.Dinosaur_Hat)
+                
+                inst[APPLE_POS] = measure()
+                move(d) # must be success
+                inst[LENGTH] = 1
 
 def create(pos, size):
     width, height = size
@@ -27,15 +60,13 @@ def create(pos, size):
         width += 1
     size = (width, width)
 
-    path = rect_path_even(size)
-
     inst = {
         PREPARE: prepare,
         TEND: tend,
 
         POS: pos,
         SIZE: size,
-        PATH: path,
+        LENGTH: 0,
     }
 
     return inst
@@ -50,7 +81,6 @@ def test():
     pos = 0, 0
     size = (L, L)
 
-    width, height = size
     inst = create(pos, size)
     drone_main(inst)
     
