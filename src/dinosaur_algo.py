7a6e99ac832c4fb29a8578e7bce540c529670299
Sze Wing Pang <davidpangsw@hotmail.com>
2025-10-31 19:36:09 -0700

    fixing dinosaur_algo...

--- src/dinosaur_algo.py
+++ src/dinosaur_algo.py
@@ -31,10 +31,13 @@ def generate_snake_path(size):
 def decide_path(pos, apple_pos, size, snake_length):
     # return hamilton_2(size)
     ax, ay = apple_pos[0] - pos[0], apple_pos[1] - pos[1]
-    l = snake_length
     w, h = size
-    k = ceildiv(l - ay, 2 * (h - 1))
+    l = snake_length
 
+    if ay == 0: # reset as (0, 1)
+        ax, ay = 0, 1
+
+    k = ceildiv(l - ay, 2 * (h - 1))
     if ax % 2 == 0:
         return hamilton((ax + 2*k, h))
     else:
